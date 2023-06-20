6b79f8c5e4b93e4c401ed1dae9110552f505abbc
Richard Wang <richardwang@88one.one>
2023-06-20 06:09:57 +0000

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.wangchong.wang',
+  url: 'https://www.wangchong.wang',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -32,7 +32,7 @@ const config = {
     locales: ['en'],
   },
 
-  scripts: [{src: "https://plausible.verystation.com/js/script.js", defer: true, 'data-domain': 'docs.wangchong.wang'}],
+  scripts: [{src: "https://plausible.verystation.com/js/script.js", defer: true, 'data-domain': 'www.wangchong.wang'}],
 
   presets: [
     [
