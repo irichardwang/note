0cf2a0d4f93938ecc812cc28ad12a0b79be1bdc6
Richard Wang <richardwang@88one.one>
2023-07-07 14:28:59 +0000

    update src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/app/intro">
+            to="/homelab/intro">
             进入我的笔记
           </Link>
         </div>
